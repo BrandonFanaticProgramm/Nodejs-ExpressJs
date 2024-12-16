9cef5ed5403f33376ff6ff26905c8655d4b58beb
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-12-16 18:11:03 -0500

    Adding example of exports

---
+++ concepts/modules/square.js
@@ -0,0 +1,10 @@
+// Assigning to exports will not modify module, must use module.exports
+module.exports = class Square {
+    constructor(width) {
+      this.width = width;
+    }
+  
+    SquareArea() {
+      return this.width ** 2;
+    }
+  };
